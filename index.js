f88854e3e5aa4c369deae562b04afff1b8e1c464
HCX <1874640833@qq.com>
2023-03-02 09:00:44 +0800

    socket.io服务端

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose")
 const userRoutes = require("./routes/userRoutes")
 const messagesRoute = require("./routes/messagesRoute")
 const app = express()
-
+const socket = require("socket.io")
 require("dotenv").config()
 
 app.use(cors())
@@ -24,4 +24,32 @@ mongoose.connect(process.env.MONGO_URL, {
 
 const server = app.listen(process.env.PORT, () => {
     console.log(`Serve Started on Port ${process.env.PORT}`)
+})
+
+const io = socket(server, {
+    cors: {
+        origin: "http://192.168.43.71:3000",
+        credentials: true,
+    }
+})
+
+// 将所有在线用户存入map中
+global.onlineUsers = new Map()
+// 首先只要有连接就会有聊天，将socket存入全局中
+io.on("connection", (socket) => {
+    global.chatSocket = socket;
+    // 只要用户登录就会触发add-user，获取用户的id和当前的socket id
+    socket.on("add-user", (userId) => {
+        onlineUsers.set(userId, socket.id)
+    })
+
+    // 当用户发送消息时候
+    socket.on("send-msg", (data) => {
+        // 在map中获取发送目标对象
+        const sendUserSocket = onlineUsers.get(data.to)
+        // 如果用户在线则推送消息
+        if (sendUserSocket) {
+            socket.to(sendUserSocket).emit("msg-recieve", data.message)
+        }
+    })
 })
